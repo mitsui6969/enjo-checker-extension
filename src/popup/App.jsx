facc240b2642a021c549ef56974e099b05fe3464
nagisa <136070735+mitsui6969@users.noreply.github.com>
2025-09-14 22:24:48 +0900

    ポップアップ表示

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function App() {
     return (
         <div>
-        <h1>炎上チェッカー🔥🔎</h1>
-        <p>
-            Twitter/Xの投稿ボタンが「炎上チェック」ボタンに変わります。
-        </p>
+            <h1>炎上チェッカー🔥🔎</h1>
+            <p>
+                Twitter/Xの投稿ボタンが「炎上チェック」ボタンに変わります。
+            </p>
         </div>
     );
 }
